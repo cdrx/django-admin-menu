308d32a7954ab4d56159243786f66101211cd47f
cdrx <cdrx@users.noreply.github.com>
2020-04-06 17:17:57 +0100

    Fixed: readme

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup
 
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
 setup(name='django-admin-menu',
-      version = __import__('admin_menu').__version__,
+      version=__import__('admin_menu').__version__,
       description='A Django admin theme with a horizontal, tabbed navigation bar',
+      long_description=readme(),
       url='http://github.com/cdrx/django-admin-menu',
       author='Chris Rose',
       license='MIT',
@@ -14,15 +21,15 @@ setup(name='django-admin-menu',
       keywords=['django', 'admin', 'theme', 'interface', 'menu', 'navigation'],
       include_package_data=True,
       classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Framework :: Django',
+          'Intended Audience :: Developers',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.5',
       ],
-)
+      )
