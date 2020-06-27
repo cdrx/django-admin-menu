62fc1cb05be02d23a531b445777346b4403c81d7
cdrx <cdrx@users.noreply.github.com>
2020-06-27 14:38:32 +0100

    Moved demo url to /admin

--- demo/kitchensink/urls.py
+++ demo/kitchensink/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.views.generic import RedirectView
+
 
 urlpatterns = [
-    url(r'^', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', RedirectView.as_view(url='/admin/')),
 ]
